11e1b5ad5d2c67c4a59d287f9a73adbe79ed646e
ASImVx0 <100488517+ASImVx0@users.noreply.github.com>
2022-02-26 22:45:06 +0400

    Create vigenere.py

---
+++ vigenere.py
@@ -0,0 +1,33 @@
+method = input("encrypt/decrypt(e/d): ")
+text = input('enter text : ')
+key = input('enter key : ')
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+for i in key:
+    if len(text) != len(key):
+        key += i
+cipher = []
+ciphertext = ''
+if method == "e":
+
+    for i in range(len(text)):
+        if text[i].isupper():
+            cipher.append((alphabet.find(text[i].lower()) + alphabet.find(key[i])) % len(alphabet))
+            ciphertext += alphabet[cipher[i]].upper()
+        elif text[i].islower():
+            cipher.append((alphabet.find(text[i]) + alphabet.find(key[i])) % len(alphabet))
+            ciphertext += alphabet[cipher[i]]
+        else:
+            ciphertext += text[i]
+elif method == "d":
+    for i in range(len(text)):
+        if text[i].isupper():
+            cipher.append((alphabet.find(text[i].lower()) - alphabet.find(key[i])) % len(alphabet))
+            ciphertext += alphabet[cipher[i]].upper()
+        elif text[i].islower():
+            cipher.append((alphabet.find(text[i]) - alphabet.find(key[i])) % len(alphabet))
+            ciphertext += alphabet[cipher[i]]
+        else:
+            ciphertext += text[i]
+else:
+    print("Please enter the correct method")
+print("our result is ", ciphertext)
